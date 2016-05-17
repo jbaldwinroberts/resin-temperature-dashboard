8b90344ec0d575be62df45abe7bacc600b0e5d98
--replace-all <joe@animustechnologies.com>
2016-05-17 12:17:42 +0100

    hey

--- libdweetAgent/Examples/dweetWriter.cpp
+++ libdweetAgent/Examples/dweetWriter.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
  cout << "DweetWriter" << endl;
  dweet = DweetApi();
  dweet.setup("sensor-monitor-4465");
- dweet.push("temp=25");
+ dweet.push("temp=30");
 
  return 0;
 }
